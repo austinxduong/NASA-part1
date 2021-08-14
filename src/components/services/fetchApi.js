cae2f8acc09786e0233809520049b06e03ff0e67
austinxduong <axduong2@gmail.com>
2021-08-14 13:09:06 -0700

    plug in nasa api

---
+++ src/components/services/fetchApi.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+export const fetchPlanets = async () => {
+  const res = await fetch('https://whispering-citadel-46770.herokuapp.com/api/v1/planets');
+  const json = await res.json();
+
+  return json;
+};
+
+export const fetchPlanet = async (id) => {
+  const res = await fetch(`https://whispering-citadel-46770.herokuapp.com/api/v1/planets/${id}`);
+  const json = await res.json();
+  
+  return json;
+};
