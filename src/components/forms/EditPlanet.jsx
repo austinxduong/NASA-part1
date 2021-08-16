ac5fbfa2e81066506d2e6e44b01322a32db50df6
austinxduong <axduong2@gmail.com>
2021-08-15 17:21:00 -0700

    create editPlanet component, next wire in details

--- src/components/forms/EditPlanet.jsx
+++ src/components/forms/EditPlanet.jsx
@@ -1,6 +1,41 @@
+/* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { usePlanet } from '../state/planet';
 import PlanetForm from './PlanetForm';
 import styles from './EditPlanet.css';
+import { updatePlanet } from '../services/planetCRUD';
 
+const EditPlanets = () => {
+  const { id } = useParams();
+  const history = useHistory();
+
+  const { loading, planet } = usePlanet(id);
+
+  const [newPlanet, setNewPlanet] = useState(null);
+
+  useEffect(() => {
+    // console.log(loading, planet);
+    if(!loading) setNewPlanet(planet);
+  }, [planet, loading]);
+
+  const handleChange = ({ target }) => {
+    setNewPlanet(prevPlanet => ({ ...prevPlanet, [target.name]:target.value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await updatePlanet({ id, ...newPlanet });
+
+    history.push(`/planets/${id}`);
+  };
+
+  if(loading && !newPlanet) return <h1>one moment please...</h1>;
+  return (
+    <section className={styles.EditPlanets}>
+      <h1>Edit!</h1>
+      <PlanetForm {...newPlanet} onChange={handleChange} onSubmit={handleSubmit} />
+    </section>
+  );
+};
+
+export default EditPlanets;
