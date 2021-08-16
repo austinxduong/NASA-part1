4f3a58877fb95102cf2756c9f06947db72456ff1
austinxduong <axduong2@gmail.com>
2021-08-15 22:05:36 -0700

    css fun

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+import Landing from './Landing';
 
 const Home = () => {
   return (
     <main className={styles.Home}>
+      <Landing />
       <h1> World 0f Galaxy</h1>
       <Link to="/planets"> Enter to see ... </Link>
     </main>
--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -6,7 +6,18 @@ const Landing = () => {
   return (
     <section className={styles.Landing}>
       <div className={styles.VideoContainer}>
-        <video playsinline muted loop autoPlay src="https://res.cloudinary.com/duwtuqr0p/video/upload/v1628986566/galaxy_r3apjr.mp4"/>
+        <video playsinline muted loop autoPlay src="https://res.cloudinary.com/duwtuqr0p/video/upload/v1628986566/galaxy_r3apjr.mp4"
+          style={{
+            position:'absolute',
+            width: '100%',
+            left: '50%',
+            top: '50%',
+            height: '100%',
+            objectFit: 'cover',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '-1'
+          }}
+        />
       </div>
     </section>
   );
