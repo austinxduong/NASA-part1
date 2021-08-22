4dabd680b9be345172d9a2d32f9277102deed5d5
austinxduong <axduong2@gmail.com>
2021-08-21 18:52:30 -0700

    refine navlink active colors

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.css';
@@ -5,9 +6,9 @@ import styles from './Header.css';
 const Header = () => {
   return <header className={styles.Header}>
     <nav>
-      <NavLink to ="/">🏠 Home </NavLink>
-      <NavLink to="/planets">   🪐  All Planets </NavLink>
-      <NavLink to="/planets/add"> ➕  Add Planet</NavLink>
+      <NavLink to ="/" exact={true} style={{ color:'#4E2C5D' }} activeStyle={{ color:'orange' }}>🏠 Home </NavLink>
+      <NavLink to="/planets" exact={true} style={{ color:'#4E2C5D' }} activeStyle={{ color:'orange' }}>   🪐  All Planets </NavLink>
+      <NavLink to="/planets/add" exact={true} style={{ color:'#4E2C5D' }} activeStyle={{ color:'orange' }}> ➕  Add Planet</NavLink>
     </nav>
   </header>;
 
