2750892fcb1ce1d557546eb2d30eb02bda3fd649
austinxduong <axduong2@gmail.com>
2021-10-23 15:07:17 -0700

    why is doing it like this :( kinda works though :(

---
+++ src/components/artist/theArtist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const theArtist = () => {
+    return (
+        <main>
+            <Link to="https://www.austinxduong.live/" </Link>
+        </main>
+    )
+}
+
+export default theArtist;
