eafb5e580ad3e8f44e05a6649bc2fefc5eb4665b
austinxduong <axduong2@gmail.com>
2022-03-30 18:55:34 -0700

    debugging CORS cross-origin fetching issue

--- src/components/services/fetchApi.js
+++ src/components/services/fetchApi.js
@@ -1,15 +1,21 @@
 /* eslint-disable max-len */
+
+// asynchronous fetch calls, returning raw json data
+
 export const fetchPlanets = async () => {
-  const res = await fetch('https://whispering-citadel-46770.herokuapp.com/api/v1/planets');
+
+  const res = await fetch('https://whispering-citadel-46770.herokuapp.com/api/v1/planets', { mode: 'cors' });
+ 
   const json = await res.json();
 
   return json;
 };
 
+
+
 export const fetchPlanet = async (id) => {
-  const res = await fetch(`https://whispering-citadel-46770.herokuapp.com/api/v1/planets/${id}`);
+  const res = await fetch(`https://whispering-citadel-46770.herokuapp.com/api/v1/planets/${id}`, { mode: 'cors' });
   const planet = await res.json();
   
   return planet;
 };
-
